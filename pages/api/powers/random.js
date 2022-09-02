86d04036f839b494d9022450c3b97346d4577808
Jefferson <jefferson-arsene-minakpon.vodounnou@epitech.eu>
2022-09-02 18:36:56 +0100

    feat: backend: add random power api route

---
+++ pages/api/powers/random.js
@@ -0,0 +1,36 @@
+import dbConnect from "../../../lib/dbConnect";
+import Power from "../../../models/Power";
+import { errorHandler } from "./index";
+
+export default async function handler(req, res) {
+  const { method } = req;
+
+  await dbConnect();
+
+  switch (method) {
+    case "GET":
+      /* GET /api/powers/random => Get a random power from database */
+      try {
+        const getRandomNumber = (min, max) => {
+          return Math.random() * (max - min) + min;
+        };
+        const randomNumber = getRandomNumber(0, 100);
+        let power = [];
+        if (randomNumber > 80) {
+          const randomPower = await Power.aggregate([{ $sample: { size: 1 } }]);
+          power = randomPower;
+        }
+        res.status(200).json({ success: true, data: power });
+      } catch (error) {
+        errorHandler(error, req, res);
+      }
+      break;
+    default:
+      /* Method not found */
+      res.status(404).json({
+        success: false,
+        message: `Method not found for route: ${req.url}.`,
+      });
+      break;
+  }
+}
