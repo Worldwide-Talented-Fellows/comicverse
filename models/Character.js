743b3ce644e2df771d707d2aad44f10ec8d81e99
Jefferson <jefferson-arsene-minakpon.vodounnou@epitech.eu>
2022-09-04 19:41:46 +0100

    feat: update api models

--- models/Character.js
+++ models/Character.js
@@ -1,19 +1,34 @@
 import mongoose from "mongoose";
 
-const CharacterSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide a name for this character."],
+const CharacterSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name for this character."],
+    },
+    description: {
+      type: String,
+      required: [true, "Please provide a description for this character."],
+    },
+    author: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Please provide an author for this character."],
+    },
+    powers: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "Power",
+    },
+    stories: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "Story",
+    },
   },
-  description: {
-    type: String,
-    required: [true, "Please provide a description for this character."],
-  },
-  powers: {
-    type: [mongoose.Schema.Types.ObjectId],
-    ref: "Power",
-  },
-});
+  {
+    /* Timestamps will automatically add createdAt and updatedAt fields to the documents. */
+    timestamps: true,
+  }
+);
 
 export default mongoose.models.Character ||
   mongoose.model("Character", CharacterSchema);
---
+++ models/Comment.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const CommentSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: [true, "Please provide a content for this comment."],
+    },
+    chapter: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Chapter",
+      required: [true, "Please provide a chapter for this comment."],
+    },
+    author: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Please provide an author for this comment."],
+    },
+  },
+  {
+    /* Timestamps will automatically add createdAt and updatedAt fields to the documents. */
+    timestamps: true,
+  }
+);
+
+export default mongoose.models.Comment ||
+  mongoose.model("Comment", CommentSchema);
