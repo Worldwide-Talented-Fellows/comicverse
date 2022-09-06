7851f2d7493be1df63b2e1dac271b8f0a7bd72a8
msbsylmz <musabsoylemez@hotmail.com>
2022-09-06 14:43:05 +0300

    Implemented Mongo Client for auth connections and adapted db to auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,14 @@
-import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import clientPromise from "../../../server/lib/mongoClient";
 
 export default NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
-    }),
-  ]
-})
+	adapter: MongoDBAdapter(clientPromise),
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_ID,
+			clientSecret: process.env.GOOGLE_SECRET,
+		}),
+	],
+});
