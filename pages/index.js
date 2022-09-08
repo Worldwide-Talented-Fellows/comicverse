03710d6e68140b19361c5e03a05d042f1a2c8781
Prince Nwaonicha <lolgfman@gmail.com>
2022-09-08 17:54:20 -0500

    Added Designers Styling to Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,55 @@
 import { useSession, signOut, signIn } from 'next-auth/react';
 import { useEffect, useState } from 'react';
-
+import styles from '../styles/home.module.css'
 export default function Home() {
-<<<<<<< HEAD
-  return <div>Welcome To The Comivcerse</div>;
-=======
-  /*THİS CODE IS FOR TESTING PURPOSE CAN BE WILL BE DELETED 
-	  YOU CAN CHECK IF THE DATABASE IS CONNECTED BY LOOKING TO CONSOLE
+  return (<div id="homepage">
+    <div id={styles.nav}>
+      Navbar
+    </div>
+    <div className={styles.heroContainer1}>
+      <h1>Trending</h1>
+      <div className={styles.heroContentContainer}>
+        <div className={styles.boxOuter}><p>The great DC Reset:Why Zaslav must Examine DC Comics Editorial</p></div>
+        <div className={styles.boxInner}><p>The great DC Reset:Why Zaslav must Examine DC Comics Editorial</p></div>
+        <div className={styles.boxOuter}><p>The great DC Reset:Why Zaslav must Examine DC Comics Editorial</p></div>
+      </div>
+    </div>
+    <div className={styles.heroContainer2}>
+      <h1>Popular</h1>
+      <div className={styles.heroContentContainer}>
+        <div className={styles.boxOuter}><p>The great DC Reset:Why Zaslav must Examine DC Comics Editorial</p></div>
+        <div className={styles.boxInner}><p>The great DC Reset:Why Zaslav must Examine DC Comics Editorial</p></div>
+        <div className={styles.boxOuter}><p>The great DC Reset:Why Zaslav must Examine DC Comics Editorial</p></div>
+      </div>
+    </div>
+    <div id={styles.laContainer}>
+      <h1>Latest Articles</h1>
+      <div>Article Content</div>
+    </div>
 
-	  YOU CAN DELETE THIS CODE IF NEEDED! 
-	*/
-  const { data: session, status } = useSession();
-  const [powers, setPowers] = useState([]);
+  </div>
+  )
+  // /*THİS CODE IS FOR TESTING PURPOSE CAN BE WILL BE DELETED 
+	//   YOU CAN CHECK IF THE DATABASE IS CONNECTED BY LOOKING TO CONSOLE
 
-  const fetchPowersData = async () => {
-    const response = await fetch(`http://localhost:3000/api/powers/random`);
-    if (response.ok) {
-      const data = await response.json();
-      setPowers(data);
-    }
-  };
+	//   YOU CAN DELETE THIS CODE IF NEEDED! 
+	// */
+  // const { data: session, status } = useSession();
+  // const [powers, setPowers] = useState([]);
 
-  useEffect(() => {
-    fetchPowersData();
-  }, []);
+  // const fetchPowersData = async () => {
+  //   const response = await fetch(`http://localhost:3000/api/powers/random`);
+  //   if (response.ok) {
+  //     const data = await response.json();
+  //     setPowers(data);
+  //   }
+  // };
 
-  console.log(powers);
+  // // useEffect(() => {
+  // //   fetchPowersData();
+  // // }, []);
+
+  // console.log(powers);
   return (
     <div>
       {session && <div>{session.user.name}</div>}
@@ -40,5 +64,4 @@ export default function Home() {
       {/* ---- Created for the auth test purposes. Will be deleted later. -----  */}
     </div>
   );
->>>>>>> 3c080bbcb9994c74405642f832c29b8f1982be55
 }
