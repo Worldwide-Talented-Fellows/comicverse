fe997edbf232eb7fa09f786375c8be498c0d0558
Ticha <tichagodwill1@gmail.com>
2022-09-11 22:12:47 +0300

    Link items for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import Link from 'next/link';
 import styles from '../../styles/home.module.css';
+import {} from 'react-icons/fa';
 
 const Navbar = () => {
-  return (
-    <div id={styles.nav}>Navbar</div>
-  )
-}
+    return (
+        <nav id={styles.nav}>
+            <span></span>
+            <Link href="">
+                <a>Home</a>
+            </Link>
+            <Link href="">
+                <a>All Comics</a>
+            </Link>
+            <Link href="">
+                <a>About</a>
+            </Link>
+            <Link href="">
+                <a>Contacts</a>
+            </Link>
+            <input type="text" name="text" placeholder="search comics" />
+            <div>
+                <h4>LogIn</h4>
+               
+            </div>
+        </nav>
+    );
+};
 
-export default Navbar
-
-
+export default Navbar;
