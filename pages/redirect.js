e53027d014f5ec5bc5d6251d68d512efb5ecef3e
Kristi <kristiana.d.ingco@gmail.com>
2022-09-12 22:41:04 -0700

    Add skeleton for redirect page

---
+++ pages/redirect.js
@@ -0,0 +1,20 @@
+const Redirect = () => {
+    return (
+        <>
+            <div>
+                <h1>Sign Up to Comicverse Account</h1>
+                <ul>
+                    <li>Create your story</li>
+                    <li>Share story</li>
+                    <li>Publish story</li>
+                </ul>
+                <div>
+                    <button>Log In</button>
+                    <button>Register</button>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Redirect;
