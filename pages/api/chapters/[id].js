9e2a3d46505df81556f64ca2ec544cb2fa2fa722
Omolara5861 <debbiegterra@gmail.com>
2022-09-15 20:04:04 +0100

    created logic to get one chapter

--- pages/api/chapters/[id].js
+++ pages/api/chapters/[id].js
@@ -1,5 +1,8 @@
 import getAuthenticatedUser from "../../../server/helpers/auth/token";
+import { NotFoundError } from "../../../server/helpers/errors";
 import dbConnect from "../../../server/lib/dbConnect";
+import Chapter from '../../../server/models/Chapter';
+
 
 export default async function handler(req, res) {
     const {
@@ -10,4 +13,21 @@ export default async function handler(req, res) {
     await dbConnect();
 
     const session = await getAuthenticatedUser(req);
+
+    switch (method) {
+        case 'GET_CHAPTER':
+            try {
+                const chapter = await Chapter.findById(id);
+                if (!chapter) {
+                    throw new NotFoundError('This chapter does not exist');
+                }
+            
+            } catch (error) {
+                
+            }
+            break;
+    
+        default:
+            break;
+    }
 }
