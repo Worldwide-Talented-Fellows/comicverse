cbb40dd27922bcd55a41fc8b633fec573fa512a4
Omolara5861 <debbiegterra@gmail.com>
2022-09-18 01:25:38 +0100

    commented code in the id file

--- pages/api/chapters/[id].js
+++ pages/api/chapters/[id].js
@@ -9,6 +9,7 @@ import Chapter from '../../../server/models/Chapter';
 
 
 export default async function handler(req, res) {
+    /** Pulling id and method from request */
     const {
         query: {
             id
@@ -16,41 +17,54 @@ export default async function handler(req, res) {
         method,
     } = req;
 
+    /** Connects to MongoDB Database */
     await dbConnect();
 
+    /** session variable to track users role */
     const session = await getAuthenticatedUser(req);
 
     switch (method) {
 
+        /** Method GET endpoint /api/chapters/:id gets and returns the chapter with a specific id from the database */
         case 'GET':
             try {
+                /** Stores the chapter with the id passed*/
                 const chapter = await Chapter.findById(id);
+                
+                /** Returns the chapter with the id passed if it exists */
                 if (chapter) {
                     res.status(200).json({
                         success: true,
                         data: chapter
                     })
                 } else {
+                    /** Otherwise this error if not found*/
                     throw new NotFoundError(`Chapter with ${id} does not exist`);
                 }
 
             } catch (err) {
+                /** Catches any error that occurs during the request and return it */
                 errorHandler(err, res)
             }
             break;
 
+        /** Method PUT endpoint /api/chapters/:id updates and returns the chapter that was updated */
         case 'PUT':
             try {
+
+                /** Pulls, updates and stores the chapter */
                 const updChapter = req.body;
                 const updatedChapter = Chapter.findByIdAndUpdate(id, updChapter, {
                     new: true,
                     runValidators: true
                 });
-
+                
+                /** Checks if user have the permision to update the chapter */
                 if (session.user?.role !== 'author' || session?.user .role !== 'moderator') {
                     throw new AuthorizationError('You have to be the author or a moderator to update this chapter');
                 }
 
+                /** Returns the updated chapter if successful */
                 if (updatedChapter) {
                     res.status(200).json({
                         success: true,
@@ -58,22 +72,29 @@ export default async function handler(req, res) {
                         data: updatedChapter
                     })
                 } else {
+                    /** Otherwise this error if not found*/
                     throw new NotFoundError(`Could not update chapter, there is no chapter with the id ${id}`);
                 }
 
             } catch (err) {
+                /** Catches any error that occurs during the request and return it */
                 errorHandler(err, res);
             }
             break;
 
+        /** Method DELETE endpoint /api/chapters/:id deletes and returns the chapter that was deleted */
         case 'DELETE':
             try {
+                
+                /** Stores the chapter to be deleted */
                 const deletedChapter = Chapter.findByIdAndDelete(id);
 
+                /** Checks if user have the permision to delete the chapter */
                 if (session?.user?.role !== 'author' || session ?.user?.role !== 'moderator') {
                     throw new AuthorizationError(`You don't have the access to delete this chapter, You have to be the author or a moderator`);
                 }
 
+                /** Returns the deleted chapter if successful */
                 if (deletedChapter) {
                     res.status(200).json({
                         success: true,
@@ -81,9 +102,11 @@ export default async function handler(req, res) {
                         data: {deletedChapter}
                     });
                 } else {
+                    /** Otherwise this error if not found*/
                     throw new NotFoundError(`Could not delete chapter, chapter with id ${id} does not exist`);
                 }
             } catch (err) {
+                /** Catches any error that occurs during the request and return it */
                 errorHandler(err, res);
             }
             break;
@@ -91,8 +114,10 @@ export default async function handler(req, res) {
         default:
             try {
                 const url = req.url;
+                /** Throws this error if a wrong method was requested */
                 throw new NotFoundError(`Could not find method for the route ${url}`);
             } catch (err) {
+                /** Catches any error that occurs during the request and return it */
                 errorHandler(err, res)
             }
             break;
