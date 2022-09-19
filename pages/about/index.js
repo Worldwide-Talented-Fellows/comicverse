ebd9ed435ef1a630d9d7673ea165fcddeb94bddd
Kristi <kristiana.d.ingco@gmail.com>
2022-09-18 23:40:25 -0700

    Add initial markup for About page

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,13 +1,66 @@
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const AboutPage = () => {
-    const router = useRouter();
-    // console.log(router);
-
     return (
         <div>
-            AboutPage
-            <button onClick={() => router.push('/')}>NAVIGATE TO HOME</button>
+            <header>
+                <span>
+                    <Link href="/">Home</Link> &gt; Home
+                </span>
+                <h1>About Us</h1>
+            </header>
+            <section>
+                <div>
+                    <h2>Vision</h2>
+                    <p>
+                        A world class model of excellence in comic
+                        administration
+                    </p>
+                </div>
+
+                <div>
+                    <h2>Mission</h2>
+                    <p>
+                        To allow everyone be comfortable to express themselves
+                        freely through writting comics such as articles,
+                        magazines, books, novels , share stories and many more.
+                    </p>
+                </div>
+
+                <div>
+                    <h2>Values</h2>
+                    <p>
+                        Our Commitment to serving our clients and users is our
+                        corporate values:
+                    </p>
+                    <ul>
+                        <li>Integrity</li>
+                        <li>Innovation</li>
+                        <li>Networking</li>
+                        <li>Professionalism</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h2>Our History</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Sed id feugiat et et nunc. Lectus ac in arcu facilisi.
+                        Id ut pellentesque tempor dignissim nisi id.
+                        Pellentesque lacus sem amet, tellus. Cras lobortis
+                        integer elit interdum risus in tincidunt aliquet.
+                        Faucibus purus facilisi bibendum cras. Est sed turpis
+                        eros, morbi velit in ligula viverra enim. Non vestibulum
+                        tristique id suspendisse scelerisque lacinia ornare
+                        faucibus. Risus sed leo, ut convallis suspendisse urna.
+                        Pulvinar blandit lorem quis proin turpis id et commodo
+                        lobortis. Nunc at curabitur velit, faucibus tristique
+                        dolor arcu nunc.
+                    </p>
+                </div>
+
+                <h2>Goals</h2>
+            </section>
         </div>
     );
 };
