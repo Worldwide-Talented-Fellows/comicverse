4010a8cf0a7cfcbecb7b5cab1d877228bf1eec00
Denis28547 <48550327+Denis28547@users.noreply.github.com>
2022-09-20 23:19:45 +0300

    restricted creating characters to authenticated users

--- pages/api/characters/index.js
+++ pages/api/characters/index.js
@@ -1,3 +1,4 @@
+import getAuthenticatedUser from '../../../server/helpers/auth/token';
 import dbConnect from '../../../server/lib/dbConnect';
 import CharacterModel from '../../../server/models/Character';
 
@@ -5,18 +6,9 @@ export default async function handler(req, res) {
     await dbConnect();
     const { method } = req;
 
+    const session = await getAuthenticatedUser(req);
+
     switch (method) {
-        case 'POST':
-            try {
-                const character = req.body;
-
-                const dbCharacter = await CharacterModel.create(character);
-
-                return res.send(dbCharacter);
-            } catch (error) {
-                return res.status(400).json({ errorMessage: error.message });
-            }
-
         case 'GET':
             try {
                 const allCharacters = await CharacterModel.find();
@@ -25,6 +17,22 @@ export default async function handler(req, res) {
                 return res.status(400).json({ errorMessage: error.message });
             }
 
+        case 'POST':
+            try {
+                if (!session) {
+                    return res
+                        .status(401)
+                        .json({ errorsMessage: 'Unauthorized' });
+                }
+
+                const character = { ...req.body, author: session.user._id };
+
+                const dbCharacter = await CharacterModel.create(character);
+
+                return res.send(dbCharacter);
+            } catch (error) {
+                return res.status(400).json({ errorMessage: error.message });
+            }
         default:
             res.status(405).json({
                 errorMessage: `No such method for this url: ${req.url}.`,
