d4cba3832ee0fcf5cfc1c0d509e49987d9694c3c
Aleksislesieur <Aleksisjonaslesieur@gmail.com>
2022-09-28 00:22:55 +0300

    landing page

---
+++ pages/landing.js
@@ -0,0 +1,25 @@
+function Landing() {
+    return (
+        <div className="mainBackground">
+            <div className="container">
+                <main>
+                    <div className="h1">Explore the world of Comic</div>
+                    <div className="h5">
+                        Read interesting stories and gain power to create your
+                        own article
+                    </div>
+                </main>
+                <section>
+                    <button>Create your story</button>
+                    <li className="li">
+                        <ul className="comic">COMIC</ul>
+                        <ul className="comic else">COMIC</ul>
+                        <ul className="comic else2">COMIC</ul>
+                    </li>
+                </section>
+            </div>
+        </div>
+    );
+}
+
+export default Landing;
