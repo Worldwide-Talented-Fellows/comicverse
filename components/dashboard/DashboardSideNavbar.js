d52adaa82dea11d2eac65c25e7d47de1ff3fc353
msbsylmz <musabsoylemez@hotmail.com>
2022-10-06 18:06:22 +0300

    update

--- components/dashboard/DashboardSideNavbar.js
+++ components/dashboard/DashboardSideNavbar.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import styles from '../dashboard/DashboardSideNavbar.module.css';
-import {useRouter} from 'next/router';
+import { useRouter } from 'next/router';
 
 import CollectionsSVG from '../../public/assets/dashboardSVG/collectionsSVG';
 import OverviewSVG from '../../public/assets/dashboardSVG/overviewSVG';
@@ -9,86 +9,113 @@ import FavoriteSVG from '../../public/assets/dashboardSVG/favoriteSVG';
 import ReviewsSVG from '../../public/assets/dashboardSVG/reviewsSVS';
 import StorySVG from '../../public/assets/dashboardSVG/storySVG';
 
-export default function DashboardSideNavbar(props) {
-  const router = useRouter();
-  const userId = router.query.userId;
-  console.log(userId);
-  return (
-    <nav className={styles.container}>
-      {/* top list  */}
-      <ul className={styles.link_container}>
-        <li className={styles.links}>
-          <Link href={`/${userId}`}>
-            <div className={styles.link_item}>
-              <OverviewSVG className={styles.svg}/>
-              <span>Overview</span>
-            </div>
-          </Link>
-        </li>
-        <li className={styles.links}>
-          <Link href={`${userId}/collections`}>
-            <div className={styles.link_item}>
-              <CollectionsSVG className={styles.svg}/>
-              <span>Collections</span>
-            </div>
-          </Link>
-        </li>
-        <li className={styles.links}>
-          <Link href={`${userId}/favourites`}>
-            <div className={styles.link_item}>
-              <FavoriteSVG className={styles.svg}/>
-              <span>Favourites</span>
-            </div>
-          </Link>
-        </li>
-        <li className={styles.links}>
-          <Link href={`${userId}/reviews`}>
-            <div className={styles.link_item}>
-              <ReviewsSVG className={styles.svg}/>
-              <span>Reviews</span>
-            </div>
-          </Link>
-        </li>
-        <li className={styles.links}>
-          <Link href={`${userId}/mystory`}>
-            <div className={styles.link_item}>
-              <StorySVG className={styles.svg}/>
-              <span>My Story</span>
-            </div>
-          </Link>
-        </li>
-        <li className={styles.links}>
-          <Link href={`${userId}/friends`}>
-            <div className={styles.link_item}>
-              <FriendsSVG className={styles.svg}/>
-              <span>Friends</span>
-            </div>
-          </Link>
-        </li>
-      </ul>
-      {/* settings  and logout list  */}
-      <ul className={styles.link_container}>
-      <li className={styles.links}>
-          <Link href={`${userId}/settings`}>
-            <div className={styles.link_item}>
-              <CollectionsSVG className={styles.svg}/>
-              <span>Settings</span>
-            </div>
-          </Link>
-        </li>
-        <li className={styles.links}>
-          <Link href='/'>
-            <div className={styles.link_item}>
-              <CollectionsSVG className={styles.svg}/>
-              <span>Log Out</span>
-            </div>
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  )
+function getListItemMap(userId) {
+    return [
+        {
+            href: `/${userId}`,
+            svg: <OverviewSVG />,
+            title: 'Overview',
+        },
+        {
+            href: `/${userId}/collections`,
+            svg: <CollectionsSVG />,
+            title: 'Collections',
+        },
+    ];
 }
 
+export default function DashboardSideNavbar(props) {
+    const router = useRouter();
+    const userId = router.query.userId;
+    const listItemsMap = getListItemMap(userId);
+
+    const listItems = listItemsMap.map((item, index) => (
+        <li key={index} className={styles.links}>
+            <Link href={item.href}>
+                <div className={styles.link_item}>
+                    {item.svg}
+                    <span>{item.title}</span>
+                </div>
+            </Link>
+        </li>
+    ));
+
+    return (
+        <nav className={styles.container}>
+            {/* top list  */}
+            <ul className={styles.link_container}>
+                {listItems}
+                {/* <li className={styles.links}>
+                    <Link href={`/${userId}`}>
+                        <div className={styles.link_item}>
+                            <OverviewSVG className={styles.svg} />
+                            <span>Overview</span>
+                        </div>
+                    </Link>
+                </li>
+                <li className={styles.links}>
+                    <Link href={`${userId}/collections`}>
+                        <div className={styles.link_item}>
+                            <CollectionsSVG className={styles.svg} />
+                            <span>Collections</span>
+                        </div>
+                    </Link>
+                </li>
+                <li className={styles.links}>
+                    <Link href={`${userId}/favourites`}>
+                        <div className={styles.link_item}>
+                            <FavoriteSVG className={styles.svg} />
+                            <span>Favourites</span>
+                        </div>
+                    </Link>
+                </li>
+                <li className={styles.links}>
+                    <Link href={`${userId}/reviews`}>
+                        <div className={styles.link_item}>
+                            <ReviewsSVG className={styles.svg} />
+                            <span>Reviews</span>
+                        </div>
+                    </Link>
+                </li>
+                <li className={styles.links}>
+                    <Link href={`${userId}/mystory`}>
+                        <div className={styles.link_item}>
+                            <StorySVG className={styles.svg} />
+                            <span>My Story</span>
+                        </div>
+                    </Link>
+                </li>
+                <li className={styles.links}>
+                    <Link href={`${userId}/friends`}>
+                        <div className={styles.link_item}>
+                            <FriendsSVG className={styles.svg} />
+                            <span>Friends</span>
+                        </div>
+                    </Link>
+                </li> */}
+            </ul>
+            {/* settings  and logout list  */}
+            <ul className={styles.link_container}>
+                <li className={styles.links}>
+                    <Link href={`${userId}/settings`}>
+                        <div className={styles.link_item}>
+                            <CollectionsSVG className={styles.svg} />
+                            <span>Settings</span>
+                        </div>
+                    </Link>
+                </li>
+                <li className={styles.links}>
+                    <Link href="/">
+                        <div className={styles.link_item}>
+                            <CollectionsSVG className={styles.svg} />
+                            <span>Log Out</span>
+                        </div>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
 
 // export  async function getStaticProps(context){
 //   const userId = context.params.userId;
--- components/dashboard/overview/Bottom.js
+++ components/dashboard/overview/Bottom.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function Bottom() {
   return (
     <div>Bottom</div>
