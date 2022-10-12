1f96e57d69d0e367e7a5bf6009b8208179a3d943
Ticha <tichagodwill1@gmail.com>
2022-10-12 22:07:25 +0300

    added SVG icons and sidebar

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,7 +7,7 @@ import { useRouter } from 'next/router';
 import styles from '../styles/navbar.module.css';
 import SearchIconSVG from '../public/assets/SearchIconSVG';
 import ProfileIconSVG from '../public/assets/ProfileIconSVG';
-import Hamburger from './Hamburger';
+import Hamburger from './HamburgerMenu/Hamburger';
 
 const Navbar = () => {
     const [searchBar, setSearchBar] = useState('');
@@ -22,45 +22,44 @@ const Navbar = () => {
 
     return (
         <div>
-        <div className={styles.navbar}>
-            <div>
-                <Link href={'/'}>LOGO</Link>
+            <div className={styles.navbar}>
+                <div>
+                    <Link href={'/'}>LOGO</Link>
+                </div>
+
+                <ul className={styles.link_container}>
+                    {Object.keys(navRoutes).map((path, index) => {
+                        let isActive = false;
+                        if (path === router.asPath) {
+                            isActive = true;
+                        }
+
+                        return (
+                            <li
+                                className={` ${styles.link_item} ${
+                                    isActive && styles.link_item_active
+                                }`}
+                                key={index}
+                            >
+                                <Link href={path}>{navRoutes[path]}</Link>
+                            </li>
+                        );
+                    })}
+                </ul>
+
+                <div className={styles.search_bar_container}>
+                    <SearchIconSVG className={styles.search_bar_svg} />
+                    <input
+                        className={styles.search_bar}
+                        value={searchBar}
+                        onChange={(e) => setSearchBar(e.target.value)}
+                        placeholder="Search comic..."
+                    />
+                </div>
+                <ProfileIconSVG />
             </div>
-
-            <ul className={styles.link_container}>
-                {Object.keys(navRoutes).map((path, index) => {
-                    let isActive = false;
-                    if (path === router.asPath) {
-                        isActive = true;
-                    }
-
-                    return (
-                        <li
-                            className={` ${styles.link_item} ${
-                                isActive && styles.link_item_active
-                            }`}
-                            key={index}
-                        >
-                            <Link href={path}>{navRoutes[path]}</Link>
-                        </li>
-                    );
-                })}
-            </ul>
-
-            <div className={styles.search_bar_container}>
-                <SearchIconSVG className={styles.search_bar_svg} />
-                <input
-                    className={styles.search_bar}
-                    value={searchBar}
-                    onChange={(e) => setSearchBar(e.target.value)}
-                    placeholder="Search comic..."
-                />
-            </div>
-            <ProfileIconSVG />
-           
+            <Hamburger />
         </div>
-         <Hamburger />
-         </div>
     );
 };
 
