8887cd530e737f729eab604a128d5501ae20dcd6
Aleksislesieur <Aleksisjonaslesieur@gmail.com>
2022-10-19 01:17:40 +0300

    added background on landing page

--- pages/landing.js
+++ pages/landing.js
@@ -1,6 +1,11 @@
 import styles from '../styles/landing.module.css';
 import Button from '../components/Button';
 
+// import React from 'react';
+import Image from 'next/image';
+import player from '../public/assets/landing_page/Player.png';
+import Star from '../public/assets/landing_page/Star.svg';
+
 function Landing() {
     return (
         <div className={styles.mainBackground}>
@@ -15,9 +20,33 @@ function Landing() {
                     </div>
                 </main>
                 <section className={styles.section}>
-                    <Button className={styles.introButton}>
-                        Create your story
-                    </Button>
+                    <Button className={styles.introButton}>Create story</Button>
+                    <div className={styles.wrappingImage}>
+                        <div>
+                            <Image
+                                className={styles.playerImage}
+                                src={player}
+                            />
+                            <div className={styles.starPlacement}>
+                                <Image
+                                    className={styles.star}
+                                    src={Star}
+                                    alt="star"
+                                />
+                            </div>
+                            <div className={styles.starPlacement2}>
+                                <Image
+                                    className={styles.star}
+                                    src={Star}
+                                    alt="star"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.starPlacement3}>
+                        <Image className={styles.star} src={Star} alt="star" />
+                    </div>
+
                     <li className={styles.listingItems}>
                         <ul className={styles.comic}>COMIC</ul>
                         <ul className={styles.comic}>COMIC</ul>
